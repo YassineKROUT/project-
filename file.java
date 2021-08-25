236430a1d033481ebdb2736ec06bc3d8bd20b36c
yassine <yassine.krout@supcom.tn>
2021-08-25 05:23:29 +0200

    the newest commit

--- file.java
+++ file.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Yassine krout"); 
+        System.out.println("Hello, World! Yassine krout\n this is the code of my application"); 
 	
     }
 }
