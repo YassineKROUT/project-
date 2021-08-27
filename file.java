b4c541a84da09f9367f7c10e5776ce94b67c861d
yassine <yassine.krout@supcom.tn>
2021-08-27 09:12:08 +0200

    ggg

--- file.java
+++ file.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        Syem.out.println("Hello, World! Yassine krout\n this is the code of my application"); 
+        System.out.println("Hello, World! Yassine krout\n this is the code of my application"); 
 	
     }
 }
